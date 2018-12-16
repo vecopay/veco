e912497400e621abf96728df3b36f613cbc7200e
Veco Coin <40065194+VecoOfficial@users.noreply.github.com>
2018-12-16 16:56:52 +0000

    [CVE-2018-17144] Fix crash bug with duplicate inputs within a transaction

--- src/validation.cpp
+++ src/validation.cpp
@@ -3176,7 +3176,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, bool fCheckPOW, bo
 
     // Check transactions
     for (const auto& tx : block.vtx)
-        if (!CheckTransaction(tx, state))
+        if (!CheckTransaction(tx, state, true))
             return error("CheckBlock(): CheckTransaction of %s failed with %s",
                 tx.GetHash().ToString(),
                 FormatStateMessage(state));
